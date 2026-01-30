18bef790fed6c2e78a402aecbcfe26f5d319477d
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-01-30 07:34:12 -0800

    Delete scripts directory

--- scripts/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-# With read_only + volume, chown on the mount is often blocked (seccomp/mount).
-# Run the app as root so it can write to VECTOR_DB_DIR (/data) without chown.
-exec "$@"
