dfbf78eb78c6d0479dbb0463a3890f43e6009e39
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-03-15 14:46:58 +0300

    извлечение текста из изображений, извлечение текста из PDFБ, подготовка текста для отправки в API;

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,12 +1,13 @@
 """Telegram bot package."""
 
 from .api_client import AskAPIClient, AskResult, AskSource
-from .service import BotReply, process_text_question
+from .service import BotReply, process_question, process_text_question
 
 __all__ = [
     "AskAPIClient",
     "AskResult",
     "AskSource",
     "BotReply",
+    "process_question",
     "process_text_question",
 ]
