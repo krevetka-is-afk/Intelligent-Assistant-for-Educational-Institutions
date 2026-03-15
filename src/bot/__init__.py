881c007a770ce75e660efa22420a4f949fea5916
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-03-15 17:14:48 +0300

    Remove unused imports from bot package initialization

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,13 +1,9 @@
 """Telegram bot package."""
 
 from .api_client import AskAPIClient, AskResult, AskSource
-from .service import BotReply, process_question, process_text_question
 
 __all__ = [
     "AskAPIClient",
     "AskResult",
     "AskSource",
-    "BotReply",
-    "process_question",
-    "process_text_question",
 ]
